be48845311654534c792761bef32561eae34d2d2
Shun Kakinoki <shunkakinoki@gmail.com>
2022-01-16 11:42:40 -0800

    perf: refactor tailwindcss color

--- packages/tailwindcss/src/index.js
+++ packages/tailwindcss/src/index.js
@@ -1,34 +1,35 @@
 const colors = require("tailwindcss/colors");
 
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   mode: "jit",
   darkMode: "class",
-  purge: ["src/**/*.{js,ts,jsx,tsx}"],
   plugins: [
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
   ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         amber: colors.amber,
-        blueGray: colors.blueGray,
-        coolGray: colors.coolGray,
         cyan: colors.cyan,
         emerald: colors.emerald,
         fuchsia: colors.fuchsia,
+        gray: colors.gray,
         indigo: colors.indigo,
         lime: colors.lime,
+        neutral: colors.neutral,
         orange: colors.orange,
         rose: colors.rose,
-        secondary: colors.purple,
+        purple: colors.purple,
         sky: colors.sky,
+        slate: colors.slate,
+        stone: colors.stone,
         teal: colors.teal,
-        trueGray: colors.trueGray,
         violet: colors.violet,
-        warmGray: colors.warmGray,
       },
     },
   },
