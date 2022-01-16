71fd2ddac2baceff7b087d8d961cea25814b7e3c
Shun Kakinoki <shunkakinoki@gmail.com>
2022-01-16 11:43:05 -0800

    fix: remove sort package json

--- packages/lint-staged/src/index.js
+++ packages/lint-staged/src/index.js
@@ -1,9 +1,5 @@
 module.exports = {
   "*.{js,ts,tsx}": ["yarn run fix:eslint", "yarn run fix:prettier"],
   "*.{md,json,yml}": ["yarn run fix:prettier"],
-  "package.json": [
-    "yarn run lint:npm-package-json",
-    "yarn run fix:sort-package-json",
-    "yarn run fix:prettier",
-  ],
+  "package.json": ["yarn run lint:npm-package-json", "yarn run fix:prettier"],
 };
